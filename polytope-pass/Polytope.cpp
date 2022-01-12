783da8aff18db79a1f7350bee4592bc931f8cc46
maximwebb <maximwebb@gmail.com>
2022-01-12 02:09:22 +0000

    Implement methods for extracting array index vectors and testing if they exhibit carrier dependencies

--- polytope-pass/Polytope.cpp
+++ polytope-pass/Polytope.cpp
@@ -1,5 +1,6 @@
 #include "Polytope.h"
 #include <iostream>
+
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
@@ -13,7 +14,7 @@
 
 using namespace llvm;
 
-bool PolytopePass::isPerfectNest(Loop& L, LoopInfo& LI, ScalarEvolution& SE) {
+bool PolytopePass::IsPerfectNest(Loop& L, LoopInfo& LI, ScalarEvolution& SE) {
 	// Will have to be changed for handing cases like for (int j = i; ...)
 	PHINode* IV = L.getInductionVariable(SE);
 	if (!IV) {
@@ -44,8 +45,8 @@ bool PolytopePass::isPerfectNest(Loop& L, LoopInfo& LI, ScalarEvolution& SE) {
 
 /* Recursively test if a value is an affine function of induction variables */
 std::optional<std::vector<int>>
-PolytopePass::isAffineValue(Value* V, const std::vector<int>& Transform, const std::vector<Value*>& Visited = {},
-							int scale = 1) {
+PolytopePass::GetAffineValue(Value* V, const std::vector<int>& Transform, const std::vector<Value*>& Visited = {},
+							 int scale = 1) {
 	/* Test for recursively defined variable in IR */
 //	if (std::find(Visited.begin(), Visited.end(), V) != Visited.end()) {
 //		return true;
@@ -62,14 +63,14 @@ PolytopePass::isAffineValue(Value* V, const std::vector<int>& Transform, const s
 	if (it != IVList.end()) {
 		std::vector<int> NewTransform;
 		NewTransform.insert(NewTransform.begin(), Transform.begin(), Transform.end());
-		int index = it - IVList.begin();
+		unsigned index = it - IVList.begin();
 		NewTransform[index] = scale;
 		return NewTransform;
 	}
 	if (isa<AddOperator>(V)) {
 		auto* AddInstr = dyn_cast<AddOperator>(V);
-		auto Fst = isAffineValue(AddInstr->getOperand(0), Transform, Visited, scale);
-		auto Snd = isAffineValue(AddInstr->getOperand(1), Transform, Visited, scale);
+		auto Fst = GetAffineValue(AddInstr->getOperand(0), Transform, Visited, scale);
+		auto Snd = GetAffineValue(AddInstr->getOperand(1), Transform, Visited, scale);
 		if (Fst && Snd) {
 			std::transform(Fst->begin(), Fst->end(), Snd->begin(), Fst->begin(), std::plus<>());
 			return Fst;
@@ -77,8 +78,8 @@ PolytopePass::isAffineValue(Value* V, const std::vector<int>& Transform, const s
 	}
 	if (isa<SubOperator>(V)) {
 		auto* SubInstr = dyn_cast<SubOperator>(V);
-		auto Fst = isAffineValue(SubInstr->getOperand(0), Transform, Visited, scale);
-		auto Snd = isAffineValue(SubInstr->getOperand(1), Transform, Visited, scale);
+		auto Fst = GetAffineValue(SubInstr->getOperand(0), Transform, Visited, scale);
+		auto Snd = GetAffineValue(SubInstr->getOperand(1), Transform, Visited, scale);
 		if (Fst && Snd) {
 			std::transform(Fst->begin(), Fst->end(), Snd->begin(), Fst->begin(), std::minus<>());
 			return Fst;
@@ -87,27 +88,40 @@ PolytopePass::isAffineValue(Value* V, const std::vector<int>& Transform, const s
 	if (isa<MulOperator>(V)) {
 		auto* MulInstr = dyn_cast<MulOperator>(V);
 		if (isa<Constant>(MulInstr->getOperand(0))) {
-			return isAffineValue(MulInstr->getOperand(1), Transform, Visited,
-								 scale * getConstantInt(MulInstr->getOperand(0)));
+			return GetAffineValue(MulInstr->getOperand(1), Transform, Visited,
+								  scale * GetConstantInt(MulInstr->getOperand(0)));
 		} else if (isa<Constant>(MulInstr->getOperand(1))) {
-			return isAffineValue(MulInstr->getOperand(0), Transform, Visited,
-								 scale * getConstantInt(MulInstr->getOperand(1)));
+			return GetAffineValue(MulInstr->getOperand(0), Transform, Visited,
+								  scale * GetConstantInt(MulInstr->getOperand(1)));
 		}
 		return {};
 	}
 	if (isa<ShlOperator>(V)) {
 		auto* ShlInstr = dyn_cast<ShlOperator>(V);
 		if (isa<Constant>(ShlInstr->getOperand(1))) {
-			auto Multiplier = 1 << getConstantInt(ShlInstr->getOperand(1));
-			return isAffineValue(ShlInstr->getOperand(0), Transform, Visited, scale * Multiplier);
+			auto Multiplier = 1 << GetConstantInt(ShlInstr->getOperand(1));
+			return GetAffineValue(ShlInstr->getOperand(0), Transform, Visited, scale * Multiplier);
+		}
+		return {};
+	}
+	/* Exclusively for testing XOR */
+	if (isa<BinaryOperator>(V)) {
+		auto* BinInstr = dyn_cast<BinaryOperator>(V);
+		/* The instruction %1 = xor k -1 simplifies to %1 = -k - 1 */
+		if (BinInstr->getOpcode() == Instruction::Xor && GetConstantInt(BinInstr->getOperand(1)) == -1) {
+			auto res = GetAffineValue(BinInstr->getOperand(0), Transform, Visited, -scale);
+			if (res) {
+				res.value().back() -= 1;
+				return res;
+			}
 		}
 		return {};
 	}
 	if (isa<CallInst>(V)) {
 		auto* FuncInstr = dyn_cast<CallInst>(V);
 		if (FuncInstr->getCalledFunction()->getName() == "llvm.smax.i32") {
-			auto Fst = isAffineValue(FuncInstr->getArgOperand(0), Transform, Visited, scale);
-			auto Snd = isAffineValue(FuncInstr->getArgOperand(1), Transform, Visited, scale);
+			auto Fst = GetAffineValue(FuncInstr->getArgOperand(0), Transform, Visited, scale);
+			auto Snd = GetAffineValue(FuncInstr->getArgOperand(1), Transform, Visited, scale);
 			if (Fst && Snd) {
 				std::transform(Fst->begin(), Fst->end(), Snd->begin(), Fst->begin(),
 							   [](int m, int n) { return m > n ? m : n; });
@@ -117,6 +131,12 @@ PolytopePass::isAffineValue(Value* V, const std::vector<int>& Transform, const s
 			return {};
 		}
 	}
+	if (isa<CastInst>(V)) {
+		auto* CastInstr = dyn_cast<CastInst>(V);
+		PrintValue(CastInstr->getOperand(0));
+		return GetAffineValue(CastInstr->getOperand(0), Transform, Visited, scale);
+	}
+
 	/* Code may be redundant */
 //	if (isa<PHINode>(V)) {
 //		std::optional<std::vector<int>> NewTransform;
@@ -125,7 +145,7 @@ PolytopePass::isAffineValue(Value* V, const std::vector<int>& Transform, const s
 //		NewVisited.insert(NewVisited.begin(), Visited.begin(), Visited.end());
 //		NewVisited.push_back(PhiInstr);
 //		for (auto& Op: PhiInstr->operands()) {
-//			NewTransform = isAffineValue(Op.get(), Transform, NewVisited);
+//			NewTransform = GetAffineValue(Op.get(), Transform, NewVisited);
 //			if (!NewTransform) {
 //				return {};
 //			}
@@ -138,24 +158,25 @@ PolytopePass::isAffineValue(Value* V, const std::vector<int>& Transform, const s
 
 /* Verifies that induction variables and array accesses are affine functions */
 /* This will initially only check for a subset of affine functions, eg. constant bounds */
-bool PolytopePass::isAffineLoop(Loop& L, LoopStandardAnalysisResults& AR) {
+bool PolytopePass::IsAffineLoop(Loop& L, LoopStandardAnalysisResults& AR) {
 	std::vector<int> InitTransform(IVList.size() + 1, 0);
 	std::vector<int> FinalTransform(IVList.size() + 1, 0);
 	for (auto& IV: IVList) {
-		auto res1 = isAffineValue(IV.init, InitTransform);
+		auto res1 = GetAffineValue(IV.init, InitTransform);
 		if (!res1) {
 			return false;
 		}
-		auto res2 = isAffineValue(IV.final, FinalTransform);
+		auto res2 = GetAffineValue(IV.final, FinalTransform);
 		if (!res2) {
 			return false;
 		}
 	}
+
 	return true;
 }
 
-bool PolytopePass::runAnalysis(Loop& L, LoopStandardAnalysisResults& AR) {
-	if (!isPerfectNest(L, AR.LI, AR.SE)) {
+bool PolytopePass::RunAnalysis(Loop& L, LoopStandardAnalysisResults& AR) {
+	if (!IsPerfectNest(L, AR.LI, AR.SE)) {
 		return false;
 	}
 	IVInfo OuterIV;
@@ -167,6 +188,8 @@ bool PolytopePass::runAnalysis(Loop& L, LoopStandardAnalysisResults& AR) {
 		return false;
 	}
 	InnerIV.loop = IL;
+	outerLoop = &L;
+	innerLoop = IL;
 
 	OuterIV.IV = OuterIV.loop->getInductionVariable(AR.SE);
 	InnerIV.IV = InnerIV.loop->getInductionVariable(AR.SE);
@@ -179,19 +202,15 @@ bool PolytopePass::runAnalysis(Loop& L, LoopStandardAnalysisResults& AR) {
 	}
 
 	InnerIV.init = &InnerBounds->getInitialIVValue();
-	InnerIV.final = &InnerBounds->getFinalIVValue();
-	InnerIV.tripCount = BinaryOperator::CreateSub(&InnerBounds->getFinalIVValue(),
-												  &InnerBounds->getInitialIVValue(),
-												  "inner.tripcount", InnerIV.loop->getLoopPreheader()->getTerminator());
 	OuterIV.init = &OuterBounds->getInitialIVValue();
+	InnerIV.final = &InnerBounds->getFinalIVValue();
 	OuterIV.final = &OuterBounds->getFinalIVValue();
-	OuterIV.tripCount = BinaryOperator::CreateSub(&OuterBounds->getFinalIVValue(),
-												  &OuterBounds->getInitialIVValue(),
-												  "outer.tripcount", InnerIV.loop->getLoopPreheader()->getTerminator());
 
 	IVList.push_back(OuterIV);
 	IVList.push_back(InnerIV);
-	if (!isAffineLoop(L, AR)) {
+
+
+	if (!IsAffineLoop(L, AR)) {
 		dbgs() << "Not affine\n";
 		return false;
 	} else {
@@ -200,20 +219,62 @@ bool PolytopePass::runAnalysis(Loop& L, LoopStandardAnalysisResults& AR) {
 	return true;
 }
 
+ArrayAssignment PolytopePass::ExtractArrayAccesses(Loop& L, LoopStandardAnalysisResults& AR) {
+	std::vector<std::vector<std::vector<int>>> readVectors;
+	/* In the future, analysis pass will ensure there is precisely one write, and perform this extraction */
+	std::vector<std::vector<int>> writeVector;
+	for (auto& Instr: *(innerLoop->getHeader())) {
+		/* Extract array access index functions for all array read/writes */
+		if (isa<StoreInst>(Instr) || isa<LoadInst>(Instr)) {
+			bool isWrite = isa<StoreInst>(Instr);
+			std::vector<int> T(IVList.size() + 1, 0);
+			auto I = Instr.getOperand(isWrite ? 1 : 0);
+			if (isa<GetElementPtrInst>(I)) {
+				auto GEPInstr = dyn_cast<GetElementPtrInst>(I);
+				auto v1 = GetAffineValue(GEPInstr->getOperand(2), T);
+				auto v2 = GetAffineValue(GEPInstr->getOperand(3), T);
+				if (v1 && v2) {
+					if (isWrite) {
+						writeVector.push_back(v1.value());
+						writeVector.push_back(v2.value());
+					} else {
+						readVectors.push_back({v1.value(), v2.value()});
+					}
+				}
+			}
+		}
+	}
+	return {writeVector, readVectors};
+}
+
+std::optional<std::vector<std::vector<int>>>
+PolytopePass::ComputeAffineTransformation(const ArrayAssignment& assignment) {
+	if (!assignment.HasLoopCarrierDependencies()) {
+		return {};
+	}
+
+
+
+	return {};
+}
 
 PreservedAnalyses PolytopePass::run(Loop& L, LoopAnalysisManager& AM, LoopStandardAnalysisResults& AR, LPMUpdater& U) {
-	if (runAnalysis(L, AR)) {
-		std::cout << "Inserted" << std::endl;
+	if (!RunAnalysis(L, AR)) {
+		return PreservedAnalyses::all();
 	}
+
+	auto assignment = ExtractArrayAccesses(L, AR);
+	auto transformation = ComputeAffineTransformation(assignment);
+
 	return PreservedAnalyses::all();
 }
 
-int PolytopePass::getConstantInt(Value* V) {
+int PolytopePass::GetConstantInt(Value* V) {
 	auto k = dyn_cast<ConstantInt>(V);
 	return k->getSExtValue();
 }
 
-void PolytopePass::printValue(Value* V) {
+void PolytopePass::PrintValue(Value* V) {
 	if (V->getName().empty()) {
 		auto k = cast<ConstantInt>(V);
 		dbgs() << k->getSExtValue() << "\n";
--- polytope-pass/Polytope.h
+++ polytope-pass/Polytope.h
@@ -2,33 +2,40 @@
 #define LLVM_TRANSFORMS_POLYLOOP_H
 
 #include <optional>
+#include <utility>
+#include "ArrayAssignment.h"
 #include "llvm/Analysis/LoopAnalysisManager.h"
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/Transforms/Utils/ValueMapper.h"
 #include "llvm/Transforms/Scalar/LoopPassManager.h"
 
+
 struct IVInfo {
 	llvm::PHINode* IV = nullptr;
 	llvm::Value* init = nullptr;
 	llvm::Value* final = nullptr;
-	llvm::Value* tripCount = nullptr;
 	llvm::Loop* loop = nullptr;
 };
 
 namespace llvm {
 	class PolytopePass : public PassInfoMixin<PolytopePass> {
 	public:
-		bool isPerfectNest(Loop& L, LoopInfo& LI, ScalarEvolution& SE);
-		bool isAffineLoop(Loop& L, LoopStandardAnalysisResults& AR);
-		bool runAnalysis(Loop& L, LoopStandardAnalysisResults& AR);
-		int getConstantInt(Value* V);
-		static void printValue(Value* V);
+		bool IsPerfectNest(Loop& L, LoopInfo& LI, ScalarEvolution& SE);
+		bool IsAffineLoop(Loop& L, LoopStandardAnalysisResults& AR);
+		bool RunAnalysis(Loop& L, LoopStandardAnalysisResults& AR);
+		int GetConstantInt(Value* V);
+		static void PrintValue(Value* V);
 		PreservedAnalyses run(Loop& L, LoopAnalysisManager& AM, LoopStandardAnalysisResults& AR, LPMUpdater& U);
 
 	private:
 		std::vector<IVInfo> IVList;
-		std::optional<std::vector<int>> isAffineValue(Value* V, const std::vector<int>& Transform, const std::vector<Value*>& Visited, int scale);
+		Loop* innerLoop;
+		Loop* outerLoop;
+		std::optional<std::vector<int>>
+		GetAffineValue(Value* V, const std::vector<int>& Transform, const std::vector<Value*>& Visited, int scale);
+		ArrayAssignment ExtractArrayAccesses(Loop& L, LoopStandardAnalysisResults& AR);
+		std::optional<std::vector<std::vector<int>>> ComputeAffineTransformation(const ArrayAssignment& assignment);
 	};
 
 } // namespace llvm
